a77bcb21e07450c022383ed63e521b18b1fdc5ad
YJChen <73424072+yujiachen99@users.noreply.github.com>
2025-01-23 11:10:58 +0800

    Add files via upload

---
+++ prompt_template.py
@@ -0,0 +1,37 @@
+
+
+repo_template = """// Description of project '{Repo_name}'.
+// {Repo_des}
+"""
+
+related_file_template = """
+// Skeleton of related files.
+// {File_related}
+"""
+
+current_file_template = """
+// Skeleton of the current file '{File_name}'.
+// Imported Files: {File_lib}
+// Fields Declarations: {File_var}
+// Functions Declarations: {File_var}
+"""
+
+called_function_template = """
+// Called functions
+// {Function_dep}
+"""
+
+api_reference_template = """
+// Reference APIs
+// {Reference_api}
+"""
+
+incomplete_function_template = """
+// Incomplete Function '{Function_name}'.
+{Incomplete_func}
+"""
+
+Skeleton_related_file_template = """
+// {file_path}
+// {file_class}
+"""
